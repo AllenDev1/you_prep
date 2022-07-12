5bcc19883dc85f202b7a4154371ac3d27a763bd0
AllenDev1 <dev.allen252@gmail.com>
2022-07-12 21:14:14 +0545

    :sparkle:  profile page added and worked on mvp

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import Home from "./pages/home.jsx";
 import Practice from "./pages/Practice.jsx";
 import Signup from "./pages/Signup.jsx";
 import Review from "./pages/Review.jsx";
+import Profile from "./pages/Profile.jsx";
 import MockInterview from "./pages/MockInterview.jsx";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Routes>
-          <Route exact index element={<Home />} />
-          <Route path="practice" element={<Practice />} />
-          <Route path="review" element={<Review />} />
-          <Route path="mock-interview" element={<MockInterview />} />
-          <Route path="sign-up" element={<Signup />} />
-        </Routes>
-      </Router>
-    </>
-  );
+	return (
+		<>
+			<Router>
+				<Routes>
+					<Route exact index element={<Home />} />
+					<Route path="practice" element={<Practice />} />
+					<Route path="review" element={<Review />} />
+					<Route path="mock-interview" element={<MockInterview />} />
+					<Route path="sign-up" element={<Signup />} />
+					<Route path="profile" element={<Profile />} />
+				</Routes>
+			</Router>
+		</>
+	);
 }
 
 export default App;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import NavBar from "../components/navbar";
 import Landingpage from "../components/Landingpage";
 import Footer from "../components/Footer";
+
 function Home() {
-  return (
-    <>
-      <NavBar />
-      <Landingpage />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<NavBar />
+			<Landingpage />
+			<Footer />
+		</>
+	);
 }
 
 export default Home;
