82f3ccd8211503ad4983b88924d123c0dc9a4075
AllenDev1 <dev.allen252@gmail.com>
2022-07-19 13:54:57 +0545

    review page and practice page

--- src/components/practice/practiceA.jsx
+++ src/components/practice/practiceA.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Col, Row, Button } from "react-bootstrap";
 import NavBar from "../navbar";
 import "./pa.scss";
-import add from "../../assets/add.svg"
+import add from "../../assets/add.svg";
 import Footer from "../Footer";
-const practiceA = () => {
+import Form from "react-bootstrap/Form";
+
+const PracticeA = () => {
+	const [showForm, setShowForm] = useState(false);
+	const ShowForm = () => {
+		setShowForm(true);
+	};
 	return (
 		<>
 			<NavBar />
@@ -31,19 +37,56 @@ const practiceA = () => {
 						<video src="..." controls></video>
 					</Col>
 				</Row>
-				<Row className="skip-diy-qus">
-					<Col className="top-qus-list">
-						<Button>Start Practicing Top Question List</Button>
-					</Col>
-					<Col className="diy-qus">
-						<Button>Skip and DIY Questions</Button>
-					</Col>
+				<Row className="skip-diy-qus add-question">
+					{!showForm ? (
+						<>
+							<Col className="top-qus-list">
+								<Button onClick={ShowForm}>
+									Start Practicing Top Question List
+								</Button>
+							</Col>
+							<Col className="diy-qus">
+								<Button>Skip and DIY Questions</Button>
+							</Col>
+						</>
+					) : (
+						<Col className="form-add-qus">
+							<Form className="add-qus-form">
+								<div className="form-wrapper-pa">
+									<Form.Group
+										className="mb-3 qus-title"
+										controlId="exampleForm.ControlInput1"
+									>
+										<Form.Control
+											type="text"
+											placeholder="Title"
+										/>
+									</Form.Group>
+									<Form.Group
+										className=" qus-dec"
+										controlId="exampleForm.ControlTextarea1"
+									>
+										<Form.Control
+											as="textarea"
+											placeholder="Describe your Interview"
+											rows={3}
+										/>
+									</Form.Group>
+								</div>
+								<Button className="lets-start-pa">
+									Let’s Start
+								</Button>
+							</Form>
+						</Col>
+					)}
 				</Row>
-                <Button className="add-question-dec"><img src={add} alt="...." /></Button>
+				<Button className="add-question-dec">
+					<img src={add} alt="...." />
+				</Button>
 			</Container>
-            <Footer/>
+			<Footer />
 		</>
 	);
 };
 
-export default practiceA;
+export default PracticeA;
